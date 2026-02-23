b6faeab93b2191e9fe42a1fc4cb870572d51c0dd
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2026-02-23 18:00:11 +0100

    #1650 Dodanie rate_limitera

--- src/Module.php
+++ src/Module.php
@@ -2,12 +2,21 @@
 
 namespace NimblePHP\Secure;
 
+use krzysztofzylka\DatabaseManager\Exception\ConnectException;
+use krzysztofzylka\DatabaseManager\Exception\DatabaseManagerException;
+use NimblePHP\Framework\Exception\DatabaseException;
+use NimblePHP\Framework\Exception\NimbleException;
 use NimblePHP\Framework\Kernel;
 use NimblePHP\Framework\Module\Interfaces\ModuleInterface;
+use NimblePHP\Framework\Module\Interfaces\ModuleUpdateInterface;
+use NimblePHP\Migrations\Exceptions\MigrationException;
+use NimblePHP\Migrations\Migrations;
 use NimblePHP\Secure\Services\ArrayService;
 use NimblePHP\Secure\Services\MysqlService;
+use NimblePHP\Secure\Services\RateLimiterService;
+use Throwable;
 
-class Module implements ModuleInterface
+class Module implements ModuleInterface, ModuleUpdateInterface
 {
 
     public function getName(): string
@@ -19,7 +28,23 @@ class Module implements ModuleInterface
     {
         Kernel::$serviceContainer->set('secure.mysql', new MysqlService());
         Kernel::$serviceContainer->set('secure.array', new ArrayService());
+        Kernel::$serviceContainer->set('secure.rateLimiter', new RateLimiterService());
         Kernel::$middlewareManager->add(new SecureMiddleware(), 5000);
+    }
+
+    /**
+     * @return void
+     * @throws DatabaseException
+     * @throws NimbleException
+     * @throws MigrationException
+     * @throws Throwable
+     * @throws ConnectException
+     * @throws DatabaseManagerException
+     */
+    public function onUpdate(): void
+    {
+        $migration = new Migrations(Kernel::$projectPath, __DIR__ . '/Migrations', 'module_secure');
+        $migration->runMigrations();
     }
 
 }
